ed2c3b622e18e10ffe8974a990bfa7397c808850
rohit <61142302+phoenixrao885@users.noreply.github.com>
2020-02-17 23:37:22 +0530

    DAY 16

---
+++ EXCEPTIONS STRING TO INTEGER.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+
+
+int main() {
+
+    try {
+
+        string str;
+
+        cin >> str;
+
+
+
+        int num = stoi(str);
+
+        cout << num;
+
+    }
+
+    catch (exception) {
+
+        cout << "Bad String";
+
+    }
+
+}
